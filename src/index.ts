63f540d7e8abbc7c3161c45988a173a0b5642ca7
divan1920 <divanmunafsalimsha@gmail.com>
2024-03-21 10:40:59 +0530

    CONNECT database

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import 'module-alias/register';
 import dotenv from 'dotenv';
 import { server } from './config/server';
+import { run } from './config/db';
 dotenv.config();
 const port = process.env.PORT || 8080;
+run();
 server().listen(port, () => console.log(`Server running on port ${port}`));
