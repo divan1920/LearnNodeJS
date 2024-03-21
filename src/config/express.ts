43e2e104b8511d75c52f0bb6cc2ff5417dda02bd
divan1920 <divanmunafsalimsha@gmail.com>
2024-03-21 10:43:21 +0530

    UPDATE cors update

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,20 +1,25 @@
 import express, { Request, Response } from 'express';
 import helmet from 'helmet';
-import { routes } from '../router';
+
 import { errorHandler } from '../middleware';
+import { routes } from '../router';
+
 export const app = () => {
   const app = express();
   app.use((_: Request, res: Response, next) => {
     res.header(
       'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Credentials, Set-Cookie'
+      'Origin, X-Requested-With, Content-Type, Accept, Credentials, Set-Cookie',
     );
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header(
       'Access-Control-Allow-Headers',
-      'Content-Type, Accept, Access-Control-Allow-Credentials, Cross-Origin'
+      'Content-Type, Accept, Access-Control-Allow-Credentials, Cross-Origin',
     );
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, OPTIONS',
+    );
     next();
   });
   app.use(express.json());
